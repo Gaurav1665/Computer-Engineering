0347ad2f1cccdf3e687276222a8162aa0ba01d4e
Ida Gaurav <127031558+Gaurav1665@users.noreply.github.com>
2025-02-17 13:40:41 +0530

    uploading Practical 19 for react calculater

---
+++ B.Tech/SEM - 4/WT - II/Practical-19/SimpleCalculter.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export default function SimpleCalculter(){
+    const [equation , setEquation] = useState("");
+
+    return(
+        <>
+            <div className="container">
+                <table className="table table-striped" style={{border: "5px solid black"}} border={3}>
+                    <thead>
+                        <tr>
+                            <th scope="col" colSpan={4}><input type="text" disabled value={equation} style={{height:"50px", width:"100%", fontSize:"20px"}} /></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="col"><button onClick={()=>setEquation("")}>CA</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation.slice(0, -1))}>C</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"%")}>%</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"/")}>/</button></td>
+                        </tr>
+                        <tr>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"7")}>7</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"8")}>8</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"9")}>9</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"*")}>*</button></td>
+                        </tr>
+                        <tr>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"4")}>4</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"5")}>5</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"6")}>6</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"-")}>-</button></td>
+                        </tr>
+                        <tr>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"1")}>1</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"2")}>2</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"3")}>3</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"+")}>+</button></td>
+                        </tr>
+                        <tr>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"00")}>00</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"0")}>0</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+".")}>.</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(eval(equation))}>=</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+}
