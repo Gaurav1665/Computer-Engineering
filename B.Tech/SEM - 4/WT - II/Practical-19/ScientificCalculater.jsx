87be3c02baa5e7c0ee87eb1da3ae0a60c8578da1
Ida Gaurav <127031558+Gaurav1665@users.noreply.github.com>
2025-02-20 11:12:10 +0530

    updating Scientific Calculter

--- B.Tech/SEM - 4/WT - II/Practical-19/ScientificCalculater.jsx
+++ B.Tech/SEM - 4/WT - II/Practical-19/ScientificCalculater.jsx
@@ -14,38 +14,44 @@ export default function ScientificCalculter(){
                     </thead>
                     <tbody>
                         <tr>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"(")}>{"("}</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+")")}>{")"}</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"%")}>%</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"/")}>/</button></td>
+                            <td scope="col" colSpan={2}><button onClick={()=>setEquation("")}>CA</button></td>
+                            <td scope="col" colSpan={2}><button onClick={()=>setEquation(equation.slice(0, -1))}>C</button></td>
                         </tr>
                         <tr>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"sin")}>sin</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"**")}>^</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(Math.sqrt(equation))}>√</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(Math.log(equation))}>log2</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+Math.PI)}>π</button></td>
+                        </tr>
+                        <tr>
+                            <td scope="col"><button onClick={()=>setEquation(Math.sin(equation))}>sin</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(Math.cos(equation))}>cos</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(Math.tan(equation))}>tan</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"%")}>%</button></td>
+                        </tr>
+                        <tr>
                             <td scope="col"><button onClick={()=>setEquation(equation+"7")}>7</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"8")}>8</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"9")}>9</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"*")}>*</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"/")}>/</button></td>
                         </tr>
                         <tr>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"cos")}>cos</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"4")}>4</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"5")}>5</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"6")}>6</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"-")}>-</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"*")}>*</button></td>
                         </tr>
                         <tr>
-                            <td scope="col"><button onClick={()=>setEquation(equation.slice(0, -1))}>C</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"1")}>1</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"2")}>2</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+"3")}>3</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"+")}>+</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"-")}>-</button></td>
                         </tr>
                         <tr>
-                            <td scope="col"><button onClick={()=>setEquation("")}>CA</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"00")}>00</button></td>
-                            <td scope="col"><button onClick={()=>setEquation(equation+"0")}>0</button></td>
                             <td scope="col"><button onClick={()=>setEquation(equation+".")}>.</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"0")}>0</button></td>
                             <td scope="col"><button onClick={()=>setEquation(eval(equation))}>=</button></td>
+                            <td scope="col"><button onClick={()=>setEquation(equation+"+")}>+</button></td>
                         </tr>
                     </tbody>
                 </table>
